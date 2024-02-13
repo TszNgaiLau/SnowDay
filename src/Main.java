08e69f6dffda6769e4247511e065adfbaba36242
TszNgaiLau <tszngail@nycstudents.net>
2024-02-13 13:31:04 -0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,40 @@
-package PACKAGE_NAME;public class Main {
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> num = new ArrayList<>();
+        try {
+            File myFile = new File("src\\nums.txt");
+            Scanner fileScanner = new Scanner(myFile);
+            while (fileScanner.hasNext()) {
+                String data = fileScanner.nextLine();
+                String[] splitData = data.split(",");
+                int sum = 0;
+                for (String splitDatum : splitData) {
+                    sum += Integer.parseInt(splitDatum);
+                }
+                num.add(sum);
+            }
+        } catch(IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+        for (int i = 0; i < num.size(); i++) {
+            int min = num.get(i);
+            int minIdx = i;
+            for (int j = i; j < num.size(); j++) {
+                if (min > num.get(j)) {
+                    minIdx = j;
+                    min = num.get(j);
+                }
+            }
+            // three lines of code to swap
+            int temp = num.get(i);
+            num.set(i, num.get(minIdx));
+            num.set(minIdx, temp);
+        }
+        System.out.println(num);
+    }
 }
